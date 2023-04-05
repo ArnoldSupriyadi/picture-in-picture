53adeb9e3836d5b0455077b8459d61171cbd9661
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-04-05 09:26:12 +0700

    finish the page

--- script.js
+++ script.js
@@ -15,5 +15,14 @@ async function selectMediaStream() {
   }
 }
 
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  //Start picture in picture
+  await VideoElement.requestPictureInPicture();
+  //reset button
+  button.disabled = false;
+});
+
 //onLoad
 selectMediaStream();
